1a657ceab5420ee21bb4aa44587ef52f63801fac
Shariful134 <shariful32213@gmail.com>
2025-04-07 23:14:01 +0600

    contact page done

--- src/app/(WithCommonLayout)/contact/page.tsx
+++ src/app/(WithCommonLayout)/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactComponents from "@/components/modules/contact/ContactComponents";
 
 const ContactPage = () => {
   return (
-    <div className="pt-20 px-10 h-[600]">
+    <div className="pt-16  px-10 h-[600]">
       <ContactComponents></ContactComponents>
     </div>
   );
--- src/components/modules/contact/ContactComponents.tsx
+++ src/components/modules/contact/ContactComponents.tsx
@@ -2,7 +2,10 @@ import photo from "../../../app/assest/images/ContactImg.png";
 import Image from "next/image";
 import Link from "next/link";
 import { FaLocationDot } from "react-icons/fa6";
-import { FaGreaterThan } from "react-icons/fa";
+import { FaClock, FaGreaterThan, FaPhone } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 const ContactComponents = () => {
   return (
@@ -44,18 +47,83 @@ const ContactComponents = () => {
           Hesitate!
         </p>
       </div>
-      <div className="mt-20 ">
-        <div>
-          <div className="flex gap-1 items-center">
-            <FaLocationDot />
+      <div className="mt-20 grid grid-cols-2 ">
+        <div className="flex flex-col gap-10">
+          <div>
+            <div className="flex gap-1 items-center">
+              <FaLocationDot />
 
-            <h2 className="text-xl text-center md:text-start font-semibold ">
-              Address
-            </h2>
+              <h2 className="text-xl text-center md:text-start font-semibold ">
+                Address
+              </h2>
+            </div>
+            <p className="text-sm md:text-sm lg:text-lg ps-5 ">
+              236 5th SE Avenue, New York NY10000, United States
+            </p>
           </div>
-          <p className="text-sm md:text-sm lg:text-lg ps-5">
-            236 5th SE Avenue, New York NY10000, United States
-          </p>
+          <div>
+            <div className="flex gap-1 items-center">
+              <FaClock />
+
+              <h2 className="text-xl text-center md:text-start font-semibold ">
+                Working Time
+              </h2>
+            </div>
+            <p className="text-sm md:text-sm lg:text-lg ps-5 ">
+              Monday-Friday: 9:00 - 22:00 Saturday-Sunday: 9:00 - 21:00
+            </p>
+          </div>
+          <div>
+            <div className="flex gap-1 items-center">
+              <FaPhone />
+
+              <h2 className="text-xl text-center md:text-start font-semibold ">
+                Phone
+              </h2>
+            </div>
+            <p className="text-sm md:text-sm lg:text-lg ps-5 ">
+              Mobile: +(84) 546-6789 Hotline: +(84) 456-6789
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div>
+            <p className="text-sm md:text-sm lg:text-lg ">Your Name</p>
+            <Input
+              type="text"
+              placeholder="name"
+              className="border-1 border-gray-400 max-w-[528px]"
+            />
+          </div>
+          <div>
+            <p className="text-sm md:text-sm lg:text-lg ">Email</p>
+            <Input
+              type="text"
+              placeholder="Email"
+              className="border-1 border-gray-400 max-w-[528px]"
+            />
+          </div>
+          <div>
+            <p className="text-sm md:text-sm lg:text-lg ">Subject</p>
+            <Input
+              type="text"
+              placeholder="subject"
+              className="border-1 border-gray-400 max-w-[528px]"
+            />
+          </div>
+          <div>
+            <p className="text-sm md:text-sm lg:text-lg ">Message</p>
+
+            <Textarea
+              placeholder="message"
+              className="border-1 border-gray-400 max-w-[528px] "
+            />
+          </div>
+          <div>
+            <Button className=" cursor-pointer px-10 hover:text-gray-900 border-0 bg-gray-300 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+              Submit
+            </Button>
+          </div>
         </div>
       </div>
     </div>
