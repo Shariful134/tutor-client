3fa23e03337353f12e532dedbded100e38c7eb0e
Shariful134 <shariful32213@gmail.com>
2025-04-11 18:41:11 +0600

    navbar changes

--- src/components/ui/shared/Navbar.tsx
+++ src/components/ui/shared/Navbar.tsx
@@ -72,16 +72,99 @@ export default function Navbar() {
   }
 
   return (
-    <header className="border-b bg-gray-200 border-gray-300 w-full fixed z-1">
-      <div className="container flex justify-between  items-center h-18 px-10 mx-auto">
-        <h1 className="text-2xl font-black flex items-center">
-          <Image src={tutorlin} width={60} height={60} alt="tutorlink"></Image>
-        </h1>
-
-        <nav className="flex gap-4 items-center">
+    <div className="navbar bg-gray-200 shadow-sm px-8 fixed z-1 ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content  bg-gray-200 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <Link
+              className={
+                pathname == "/"
+                  ? "text-purple-600 underline text-sm "
+                  : "text-black text-sm  hover:text-purple-600 hover:underline"
+              }
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={
+                pathname == "/tutors"
+                  ? "text-purple-600 underline text-sm  "
+                  : "text-black text-sm  hover:text-purple-600 hover:underline"
+              }
+              href="/tutors"
+            >
+              Tutors
+            </Link>
+            <Link
+              className={
+                pathname == "/contact"
+                  ? "text-purple-600 underline text-sm  "
+                  : "text-black text-sm  hover:text-purple-600  hover:underline"
+              }
+              href="/contact"
+            >
+              Contact
+            </Link>
+            <Link
+              className={
+                pathname == "/about"
+                  ? "text-purple-600 underline text-sm  "
+                  : "text-black text-sm  hover:text-purple-600 hover:underline"
+              }
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className={
+                pathname == "/blog"
+                  ? "text-purple-600 underline text-sm  "
+                  : "text-black text-sm  hover:text-purple-600 hover:underline "
+              }
+              href="/blog"
+            >
+              Blogs{" "}
+            </Link>
+          </ul>
+        </div>
+        <a className=" text-xl">
+          <Image
+            className="hidden lg:inline"
+            src={tutorlin}
+            width={60}
+            height={60}
+            alt="tutorlink"
+          ></Image>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 flex justify-center items-center gap-5">
           <Link
             className={
-              pathname == "/" ? "text-purple-600 underline" : "text-black"
+              pathname == "/"
+                ? "text-purple-600 underline text-lg "
+                : "text-black text-lg hover:text-purple-600 hover:underline"
             }
             href="/"
           >
@@ -89,7 +172,9 @@ export default function Navbar() {
           </Link>
           <Link
             className={
-              pathname == "/tutors" ? "text-purple-600 underline" : "text-black"
+              pathname == "/tutors"
+                ? "text-purple-600 underline text-lg "
+                : "text-black text-lg hover:text-purple-600 hover:underline"
             }
             href="/tutors"
           >
@@ -98,8 +183,8 @@ export default function Navbar() {
           <Link
             className={
               pathname == "/contact"
-                ? "text-purple-600 underline"
-                : "text-black"
+                ? "text-purple-600 underline text-lg "
+                : "text-black text-lg hover:text-purple-600 hover:underline"
             }
             href="/contact"
           >
@@ -107,7 +192,9 @@ export default function Navbar() {
           </Link>
           <Link
             className={
-              pathname == "/about" ? "text-purple-600 underline" : "text-black"
+              pathname == "/about"
+                ? "text-purple-600 underline text-lg "
+                : "text-black text-lg hover:text-purple-600 hover:underline"
             }
             href="/about"
           >
@@ -115,74 +202,77 @@ export default function Navbar() {
           </Link>
           <Link
             className={
-              pathname == "/blog" ? "text-purple-600 underline" : "text-black"
+              pathname == "/blog"
+                ? "text-purple-600 underline text-lg "
+                : "text-black text-lg hover:text-purple-600 hover:underline"
             }
             href="/blog"
           >
             Blogs{" "}
           </Link>
-
-          {user ? (
-            <>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Avatar className="size-10 rouded-full ">
-                    {profileImg ? (
-                      <AvatarImage src={profileImg} />
-                    ) : (
-                      <AvatarImage src="https://github.com/shadcn.png" />
-                    )}
-                  </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="border border-gray-400 bg-white mt-2 ">
-                  <DropdownMenuLabel className="font-semibold">
-                    <div className="flex justify-center flex-col items-center gap-2">
-                      <Avatar className="size-10 rouded-full ">
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                      </Avatar>
-                      <Link href={`/${role}/dashboard`}>
-                        <Button className="roudend-full border-0 btn cursor-pointer bg-gray-300 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
-                          {" "}
-                          View Profile
-                        </Button>
-                      </Link>
-                    </div>
-                  </DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-
-                  <Link href={`/${role}/dashboard`}>
-                    <DropdownMenuItem className="hover:bg-gray-200 px-3 py-2 cursor-pointer">
-                      Dashboard
-                    </DropdownMenuItem>
-                  </Link>
-                  <Link href={`/${role}/bookings`}>
-                    <DropdownMenuItem className="hover:bg-gray-200 px-3 py-2 cursor-pointer">
-                      Bookings
-                    </DropdownMenuItem>
-                  </Link>
-                  <DropdownMenuSeparator className="border-b border-gray-400" />
-                  <DropdownMenuItem
-                    className="hover:bg-gray-200 px-3 py-2 cursor-pointer"
-                    onClick={handleLogOut}
-                  >
-                    <LogOut /> LogOut
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </>
-          ) : (
-            <Link href={"/login"}>
-              <Button
-                variant="outline"
-                className="roudend-full btn cursor-pointer border-0 bg-gray-300 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ..."
-              >
-                SignIn
-              </Button>
-            </Link>
-          )}
-        </nav>
+        </ul>
       </div>
-    </header>
+      <div className="navbar-end">
+        {user ? (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar className="size-10 rouded-full ">
+                  {profileImg ? (
+                    <AvatarImage src={profileImg} />
+                  ) : (
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                  )}
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="border border-gray-400 bg-white mt-2 ">
+                <DropdownMenuLabel className="font-semibold">
+                  <div className="flex justify-center flex-col items-center gap-2">
+                    <Avatar className="size-10 rouded-full ">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <Link href={`/${role}/dashboard`}>
+                      <Button className="roudend-full border-0 btn cursor-pointer bg-gray-300 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
+                        {" "}
+                        View Profile
+                      </Button>
+                    </Link>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                <Link href={`/${role}/dashboard`}>
+                  <DropdownMenuItem className="hover:bg-gray-200 px-3 py-2 cursor-pointer">
+                    Dashboard
+                  </DropdownMenuItem>
+                </Link>
+                <Link href={`/${role}/bookings`}>
+                  <DropdownMenuItem className="hover:bg-gray-200 px-3 py-2 cursor-pointer">
+                    Bookings
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator className="border-b border-gray-400" />
+                <DropdownMenuItem
+                  className="hover:bg-gray-200 px-3 py-2 cursor-pointer"
+                  onClick={handleLogOut}
+                >
+                  <LogOut /> LogOut
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : (
+          <Link href={"/login"}>
+            <Button
+              variant="outline"
+              className="roudend-full btn cursor-pointer border-0 bg-gray-300 text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ..."
+            >
+              SignIn
+            </Button>
+          </Link>
+        )}
+      </div>
+    </div>
   );
 }
